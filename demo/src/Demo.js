063374eb29ff242bb514cca26348c9c8fb979c54
m_srinivas <cnu.prince8@gmail.com>
2021-09-30 10:16:02 +0530

    created react project

---
+++ demo/src/Demo.js
@@ -0,0 +1,13 @@
+import Nav from "./Navigation/Nav";
+
+function Demo() {
+    return(
+        <div>
+            <Nav />
+            <h1>Demo</h1>
+
+        </div>
+    )
+}
+
+export default Demo;
---
+++ demo/src/Navigation/Nav.js
@@ -0,0 +1,9 @@
+const Nav = () => {
+    return(
+        <div>
+            <h1> Header </h1>
+        </div>
+    )
+}
+
+export default Nav;
