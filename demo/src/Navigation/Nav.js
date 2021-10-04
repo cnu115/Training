192759b3cc4e2224c68ce9d81e4c64258bdfc0a5
m_srinivas <cnu.prince8@gmail.com>
2021-10-04 10:12:58 +0530

    changes

--- demo/src/Navigation/Nav.js
+++ demo/src/Navigation/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <Link to="/">Home</Link>
+                    {/*<Link to="/">Demo</Link>*/}
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
@@ -13,8 +13,8 @@ const Nav = () => {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <li>
+                                <Link to="/" className="nav-link" aria-current="page">Home</Link>
                             </li>
                             <li className="nav-item">
                                 <Link className="nav-link" aria-current="page" to="/Faq">FAQ</Link>
--- demo/src/Pages/Home.js
+++ demo/src/Pages/Home.js
@@ -1,9 +1,35 @@
-const Home = () => {
-    return(
-        <div>
-            <h1>Home page</h1>
-        </div>
-    )
-}
+// const Home = () => {
+//     return(
+//         <div>
+//             <h1>Home page</h1>
+//         </div>
+//     )
+// }
 
+import React, {Component} from 'react';
+class Home extends Component{
+    state = {
+        a:1,
+        arr: []
+    }
+    add = () => {
+        // alert('alert')
+        this.setState({
+            a: 4,
+            arr: [2,3]
+        })
+    }
+    render() {
+        // console.log(this.state)
+        return(
+            <div>
+                Home page <br />
+
+                <span>a value {this.state.a}</span> <br />
+
+                <button onClick={this.add}>add</button>
+            </div>
+        )
+    }
+}
 export default Home;
