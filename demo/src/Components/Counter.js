ba0573cd49ce913245cdbd188051aa8aac341d69
m_srinivas <cnu.prince8@gmail.com>
2021-10-29 12:00:36 +0530

    css changes

--- demo/src/Components/Counter.js
+++ demo/src/Components/Counter.js
@@ -1,17 +1,26 @@
 import React from "react";
 import {connect} from 'react-redux';
 import Actions from '../redux/action';
+import './counter.css';
 const Counter = (props) => {
     console.log(props)
     return(
-        // <div className="page-content page-container" id="page-content">
+
             <div className="padding">
-                <div className="row d-flex justify-content-center">
+                <div className="row text-center">
+                    {/*<div className="col-sm-5">*/}
+                    {/*    Counter : {props.counter}*/}
+                    {/*</div>*/}
                     <div className="col-sm-12">
-                Counter : {props.counter}
+                        <div className="counter">
+                            <i className="fa fa-lightbulb-o fa-2x" />
+                            <h2 className="timer count-title count-number" data-to="11900" data-speed="1500" />
+                            <p className="count-text ">{props.counter}</p>
+                        </div>
                     </div>
                 </div>
-                <div className="row d-flex justify-content-center">
+
+                <div className="row text-center">
                     <div className="col-sm-6">
                         <button type="button" onClick={() => props.increaseCounter()} className="btn btn-primary">Increase counter</button>
                     </div>
@@ -20,7 +29,7 @@ const Counter = (props) => {
                     </div>
                 </div>
             </div>
-        // </div>
+
     )
 }
 
